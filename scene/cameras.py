a532426eefee2f098c7a935ca5074204c240a175
smbonilla <sierrabonilla14@gmail.com>
2024-01-25 13:50:43 +0000

    ignore lpipsPyTorch

--- scene/cameras.py
+++ scene/cameras.py
@@ -15,7 +15,7 @@ import numpy as np
 from utils.graphics_utils import getWorld2View2, getProjectionMatrix
 
 class Camera(nn.Module):
-    def __init__(self, colmap_id, R, T, FoVx, FoVy, image, gt_alpha_mask,
+    def __init__(self, colmap_id, R, T, FoVx, FoVy, image, depth, gt_alpha_mask,
                  image_name, uid,
                  trans=np.array([0.0, 0.0, 0.0]), scale=1.0, data_device = "cuda"
                  ):
@@ -37,6 +37,7 @@ class Camera(nn.Module):
             self.data_device = torch.device("cuda")
 
         self.original_image = image.clamp(0.0, 1.0).to(self.data_device)
+        self.original_depth = depth.to(self.data_device)
         self.image_width = self.original_image.shape[2]
         self.image_height = self.original_image.shape[1]
 
@@ -46,7 +47,7 @@ class Camera(nn.Module):
             self.original_image *= torch.ones((1, self.image_height, self.image_width), device=self.data_device)
 
         self.zfar = 100.0
-        self.znear = 0.01
+        self.znear = 1.0 # changed from 0.01 -> 1.0
 
         self.trans = trans
         self.scale = scale
