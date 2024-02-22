de47ca3be8da5edcb8c4e8b9c092474967922a33
Sierra <rmapsmb@ucl.ac.uk>
2024-02-22 17:38:04 +0000

    not sure what I changed

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -84,13 +84,13 @@ class OptimizationParams(ParamGroup):
         self.percent_dense = 0.01
 
         self.lambda_dssim = 0.2
-        self.lambda_norm = 0.3
-        self.lambda_depth = 0.5
+        self.lambda_norm = 0.2
+        self.lambda_depth = 0.6
         
         self.densification_interval = 100
         self.opacity_reset_interval = 3000
         self.densify_from_iter = 500
-        self.densify_until_iter = 4_000
+        self.densify_until_iter = 6_000
         self.densify_grad_threshold = 0.0002
         super().__init__(parser, "Optimization Parameters")
 
